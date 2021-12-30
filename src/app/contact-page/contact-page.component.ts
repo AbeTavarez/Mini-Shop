80673085764a0bf7f0871853007eb11d9849e522
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-29 21:10:03 -0500

    Send message to seller

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { dummyData } from '../dummy-data';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  email: string = '';
+  message: string = '';
+  listing: Listing;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.listing = dummyData.find(listing => listing.id === id);
+
+    // message
+    this.message = `Hello, I'm interested in your ${this.listing.name.toLowerCase()!}`;
+  }
+
+  sendMessage(): void {
+    alert(`Your message has been sent!`);
+    this.router.navigateByUrl('/listings');
   }
 
 }
