20fa2ca00cc4390e7fcb889e5c6eb99f97ca4409
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-01 18:54:33 -0500

    Added a views property in the Listing type and on the fake data file

--- src/app/dummy-data.ts
+++ src/app/dummy-data.ts
@@ -5,30 +5,35 @@ export const dummyData: Listing[] = [
         id: '123',
         name: 'Old Boat',
         description: 'A very old boat',
-        price: 800
+        price: 800,
+        views: 0
     },
     {
         id: '345',
         name: 'New Boat',
         description: 'A brand new boat',
-        price: 10_000
+        price: 10_000,
+        views: 0
     },
     {
         id: '567',
         name: 'Boots',
         description: 'Black boots',
-        price: 300
+        price: 300,
+        views: 0
     },
     {
         id: '789',
         name: 'Hat',
         description: 'A red hat',
-        price: 400
+        price: 400,
+        views: 0
     },
     {
         id: '901',
         name: 'Watch',
         description: 'Gold watch',
-        price: 15_000
+        price: 15_000,
+        views:0
     },
 ]
