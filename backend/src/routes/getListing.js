65f0a00b630552a3d5e7460acc829c21f958e604
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-01 15:04:12 -0500

    querying single listing from database

--- backend/src/routes/getListing.js
+++ backend/src/routes/getListing.js
@@ -1,12 +1,17 @@
-import Boom  from "@hapi/boom";
+import  Boom  from "@hapi/boom";
 import { dummyData } from "./dummyData";
+import { db } from "../db";
 
 export const getListingRoute = {
     method: 'GET',
     path: '/api/listings/{id}',
-    handler: (req, h) => {
+    handler: async (req, h) => {
         const id = req.params.id;
-        const listing = dummyData.find(listing => listing.id === id);
+        const { results } = await db.query(
+            'SELECT * FROM listings WHERE id=?',
+            [id],
+        );
+        const listing = results[0];
         if (!listing) throw Boom.notFound(`Listing does not exist with id ${id}`);
         return listing;
     }
