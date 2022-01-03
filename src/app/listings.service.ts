3e0a9d0fcff824769f483a7321b49b5b18438601
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-02 20:35:15 -0500

    Service methods for deleting and getting user own listings

--- src/app/listings.service.ts
+++ src/app/listings.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Listing } from './types';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +18,32 @@ export class ListingsService {
     private http:HttpClient
   ) { }
 
+  // ============= Gets all Listings
   getListings(): Observable<Listing[]> {
     return this.http.get<Listing[]>('/api/listings');
+  };
+
+  // ============ Get single listing
+  getListingById(id: string): Observable<Listing> {
+    return this.http.get<Listing>(`/api/listings/${id}`);
+  };
+
+  // ============ Increases view count on listing
+  addViewToListing(id: string): Observable<Listing> {
+    return this.http.post<Listing>(
+      `/api/listings/${id}/add-view`,
+      {},
+      httpOptions
+      )
+  };
+
+  // ============ Get all listings for a specific user
+  getListingsForUser(): Observable<Listing[]> {
+    return this.http.get<Listing[]>('/api/users/12345/listings');
+  };
+
+  // ============ Deletes a listing
+  deleteListing(id: string): Observable<any> {
+    return this.http.delete(`/api/listings/${id}`);
   }
 };
