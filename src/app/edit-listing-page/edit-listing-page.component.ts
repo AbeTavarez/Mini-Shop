797c1d24537619fea09d3e7c0b1c7ea9d368f322
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-02 21:55:42 -0500

    Able to edit listings from db

--- src/app/edit-listing-page/edit-listing-page.component.ts
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Listing } from '../types';
-import { dummyData } from '../dummy-data';
+import { ListingsService } from '../listings.service';
 
 @Component({
   selector: 'app-edit-listing-page',
@@ -12,17 +12,22 @@ export class EditListingPageComponent implements OnInit {
   listing: Listing;
   constructor(
     private router: Router, 
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private listingsService: ListingsService
+    ) {
 
     }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.listing = dummyData.find(listing => listing.id === id);
-  }
+    this.listingsService.getListingById(id)
+    .subscribe(listing => this.listing = listing)
+  };
 
-  onSubmit(): void {
-    alert(`Saving changes to the listing...`);
-    this.router.navigateByUrl('/my-listings');
-  }
-}
+  onSubmit({name, description, price}): void {
+    this.listingsService.editListing(this.listing.id, name, description, price)
+    .subscribe(() => {
+      this.router.navigateByUrl('/my-listings');
+    });
+  };
+};
