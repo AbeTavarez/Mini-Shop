e6c021581d8636f75ee80f31eac24ed77220e83f
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-02 20:12:20 -0500

    using service file

--- src/app/listing-details/listing-details.component.ts
+++ src/app/listing-details/listing-details.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { dummyData } from '../dummy-data';
+import { ListingsService } from '../listings.service';
 import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-details',
   templateUrl: './listing-details.component.html',
-  styleUrls: ['./listing-details.component.css']
+  styleUrls: ['./listing-details.component.css'],
 })
 export class ListingDetailsComponent implements OnInit {
+  isLoading: boolean = true;
   listing: Listing;
 
   constructor(
     private route: ActivatedRoute,
-  ) { }
+    private listingService: ListingsService
+  ) {}
 
   ngOnInit(): void {
     // gets the is from url (ActivatedRoute)
     const id = this.route.snapshot.paramMap.get('id');
-    // finds the corresponding listing
-    this.listing = dummyData.find(listing => listing.id === id)
-  }
+    // finds the corresponding listing from db
+    this.listingService.getListingById(id).subscribe((listing) => {
+      this.listing = listing;
+      this.isLoading = false;
+    });
 
+    // adds a view to listing
+    this.listingService
+      .addViewToListing(id)
+      .subscribe(() => console.log(`Views updated`));
+  }
 }
