de5889db9466bf4ca308886047f693fb3f4ad7f1
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-03 23:12:51 -0500

    verifying user and token

--- backend/src/routes/createNewListing.js
+++ backend/src/routes/createNewListing.js
@@ -1,14 +1,20 @@
 import { v4 as uuid } from "uuid";
+import * as admin from 'firebase-admin';
 import { db } from "../db";
 
 export const createNewListingRoute = {
   method: "POST",
   path: "/api/listings",
   handler: async (req, h) => {
+    const token = req.headers.authtoken;
+    const user = await admin.auth().verifyIdToken(token);
     const id = uuid(); // new id for listing
+    let userId;
+    if(user) {
+      userId = user.user_id;
+    }
     // destructure all info from payload
     const { name = "", description = "", price = 0 } = req.payload;
-    const userId = '12345'; //! TODO: change on auth
     const views = 0;
 
     // Query
